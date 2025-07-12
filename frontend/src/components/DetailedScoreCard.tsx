f469e277ca91ac4533c1099b8f85a8dec7068cb4
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-07-12 22:25:11 +0530

    updated detailed score card

--- frontend/src/components/DetailedScoreCard.tsx
+++ frontend/src/components/DetailedScoreCard.tsx
@@ -1,13 +1,59 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Info from "./Info";
+import Commentry from "./Commentry";
+import Scoreboard from "./Scoreboard";
 
 const DetailedScoreCard = ({ matchId }: { matchId: string | undefined }) => {
-  const navigate = useNavigate()
+  const [currentState, setCurrentState] = useState<String>("info");
   return (
     <div>
-      <div>hii i am detailed score of matches {matchId}</div>
-      <button onClick={()=>{
-        navigate("/matches")
-      }} >back</button>
+      <div className="border mx-3 mt-5  ">
+        <div className="flex justify-between bg-gradient-to-r from-[#e53935] to-[#b71c1c] text-white p-4  shadow-md m-auto mt-3  border w-[70%] py-2 px-5  ">
+          <h1>Team1</h1>
+          <p>vs</p>
+          <h1>Team 2</h1>
+        </div>
+        <div className="flex  justify-center mb-2  ">
+          <ul className="flex   text-sm  mt-4   ">
+            <li className="  ">
+              <button
+                onClick={() => {
+                  setCurrentState("info");
+                }}
+                className=" px-4 py-2 border "
+              >
+                Info
+              </button>
+            </li>
+            <li className="">
+              <button
+                onClick={() => {
+                  setCurrentState("commentry");
+                }}
+                className="   border border-black  px-4 py-2    "
+              >
+                commentry
+              </button>
+            </li>
+            <li className="">
+              <button
+                onClick={() => {
+                  setCurrentState("scoreboard");
+                }}
+                className=" px-4 py-2 border "
+              >
+                scorecard
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="text-center border mx-3 mb-3  ">
+          {currentState === "info" && <Info />}
+          {currentState === "commentry" && <Commentry />}
+          {currentState === "scoreboard" && <Scoreboard />}
+        </div>
+      </div>
     </div>
   );
 };
